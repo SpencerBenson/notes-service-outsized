a29f36b6b99fde4cf1984105c9b8f3c5846f918c
Benson N Kariuki <bennspencer@gmail.com>
2023-12-08 18:22:49 +0300

    Online db

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 
 // MongoDB Connection
-const dbConnection  = mongoose.connect(process.env.mongoDb_URL as string, {} as mongoose.ConnectOptions);
+//const dbConnection  = mongoose.connect(process.env.mongoDb_URL as string, {} as mongoose.ConnectOptions);
+const dbConnection  = mongoose.connect(process.env.mongoDb_URL_ATLAS as string, {} as mongoose.ConnectOptions);
  
 export default dbConnection ;
